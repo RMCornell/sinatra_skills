f02cba92e531376b9f12d63bd37c4f84e3ae08b6
RMCornell <zen.alchemist5280@gmail.com>
2015-05-19 18:32:18 -0600

    Create Method created

--- app/models/skill_inventory.rb
+++ app/models/skill_inventory.rb
@@ -1,5 +1,17 @@
+require 'yaml/store'
+require_relative 'skill'
+
 class SkillInventory
   def self.database
     @database ||= YAML::Store.new("db/skill_inventory")
+  end
+
+  def self.create(skill)
+    database.transaction do
+      database['skills'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['skills'] << {'id' => database['total'], 'name' => skill['name'], 'description' => skill['description']}
+    end
   end
 end
